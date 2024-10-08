c5a32c2c121565904a1c09bfada2c5b610a2fb18
lorna gathumbi <nyawiralornah@gmail.com>
2024-10-08 12:29:01 +0300

    js event acting

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+const DODGER = document.getElementById('dodger');
+function moveDodgerLeft() {
+    const leftNumbers = DODGER.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      DODGER.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    const leftNumbers = DODGER.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+    if (left <180) {
+        DODGER.style.left = `${left + 1}px`;
+      }
+  }
